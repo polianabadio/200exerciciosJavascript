840cafc8edd1b4018f8e7fde434a3f1573c32211
Poliana Ferreira Abadio <polianabadio@gmail.com>
2025-09-15 11:10:13 -0300

    Escreva um programa que verifica se um número está dentro de um determinado intervalo.

---
+++ exercicio19.js
@@ -0,0 +1,9 @@
+//Escreva um programa que verifica se um número está dentro de um determinado intervalo.
+
+let num = prompt("Digite um número de 0 a 10:");
+
+if(num>=0 && num<=10){
+    console.log("O número está dentro do intervalo!");
+}else{
+    console.log("O número não está dentro do intervalo!");
+}
