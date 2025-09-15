d9400f1d4a7eb05fecf7cc8f365913b28c627951
Poliana Ferreira Abadio <polianabadio@gmail.com>
2025-09-15 11:07:51 -0300

    Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.

---
+++ exercicio18.js
@@ -0,0 +1,12 @@
+//Escreva um programa que verifica se uma pessoa pode dirigir com base na idade.
+
+function maiorIdade(idade){
+    if(idade>=18){
+        console.log("Você já pode tirar a sua habilitação para dirigir!")
+    }else{
+        console.log("Você não tem idade para tirar a sua habilitação para dirigir!")
+    }
+}
+
+let idade = prompt("Digite a sua idade:");
+maiorIdade(idade);
