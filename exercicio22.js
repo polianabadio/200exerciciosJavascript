25771ff4602d9a0d8c1c6a8288c97ec7a607f842
Poliana Ferreira Abadio <polianabadio@gmail.com>
2025-09-15 11:45:17 -0300

    Escreva um programa que determina o número de dias em um mês.

---
+++ exercicio22.js
@@ -0,0 +1,16 @@
+//Escreva um programa que determina o número de dias em um mês.
+
+let mes = prompt("Digite o mês:");
+
+let nomeMes = mes.toLocaleLowerCase();
+
+if(nomeMes==="janeiro" || nomeMes==="março" || nomeMes==="maio" || nomeMes==="julho" || nomeMes==="agosto" 
+    || nomeMes==="outubro" || nomeMes==="dezembro"){
+        console.log("O mês tem 31 dias!");
+    } else if(nomeMes==="abril" || nomeMes==="junho" || nomeMes==="setembro" || nomeMes==="novembro"){
+        console.log("O mês tem 30 dias!");
+    }else if(nomeMes==="fevereiro"){
+        console.log("O mês tem 29 dias se o ano for bissexto e 28 se o ano não for bissexto!");
+    }else{
+        console.log("Mês inválido!")
+    }
