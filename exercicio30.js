751a0bc3c83f2b0204fda38dea897e3eab9f4c12
Poliana <polianabadio@gmail.com>
2025-09-17 16:44:39 -0300

    Usando um laço for, imprima todos os números primos de 1 a 100.

--- exercicio30.js
+++ exercicio30.js
@@ -1,2 +1,16 @@
 //Usando um laço for, imprima todos os números primos de 1 a 100.
 
+for (let num = 2; num <= 100; num++) {
+    let primo = true;
+  
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        primo = false;
+        break;
+      }
+    }
+  
+    if (primo) {
+      console.log(num);
+    }
+  }
