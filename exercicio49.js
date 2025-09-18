d6c909c04a91fd660c1ffb0febd889d4e7d6215c
Poliana <polianabadio@gmail.com>
2025-09-18 17:32:50 -0300

    Escreva uma função que aceite três números como argumentos e retorne o maior deles.

---
+++ exercicio49.js
@@ -0,0 +1,8 @@
+//Escreva uma função que aceite três números como argumentos e retorne o maior deles.
+
+function numMaior(num1,num2,num3){
+    let maior = Math.max(num1, num2, num3);
+    return maior;
+}
+
+numMaior(10,5,3);
