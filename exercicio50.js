573695d941b043dbc3fb7779e509e8e6d28c9573
Poliana <polianabadio@gmail.com>
2025-09-18 18:29:14 -0300

    Escreva uma função que calcule o fatorial de um número.

--- exercicio50.js
+++ exercicio50.js
@@ -1,2 +1,8 @@
 //Escreva uma função que aceite uma string como argumento e retorne a string invertida.
 
+function inversaoTexto(texto){
+    let textoInvertido = texto.split("").reverse().join("");
+    return textoInvertido
+}
+
+inversaoTexto("Javascript");
