08c8ad39e8125960979890048bb469fce633ad2d
Poliana <polianabadio@gmail.com>
2025-09-18 17:26:42 -0300

    Escreva uma função que aceite um número como argumento e retorne o seu quadrado.

---
+++ exercicio47.js
@@ -0,0 +1,8 @@
+// Escreva uma função que aceite um número como argumento e retorne o seu quadrado.
+
+function quadrado(num){
+    let numQuadrado = num**2;
+    return numQuadrado;
+}
+
+quadrado(5);
