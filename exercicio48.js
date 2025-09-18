3060f6665ea2d4cdabc31536eb2dab98ee257118
Poliana <polianabadio@gmail.com>
2025-09-18 17:30:05 -0300

    Escreva uma função que aceite um número como argumento e retorne se ele é par ou ímpar.

---
+++ exercicio48.js
@@ -0,0 +1,13 @@
+//Escreva uma função que aceite um número como argumento e retorne se ele é par ou ímpar.
+
+function parOuImpar(num){
+    let texto = ""
+    if(num%2===0){
+        texto = `O número ${num} é par`;
+    }else{
+        texto = `O número ${num} é ímpar`;
+    }
+    return texto;
+}
+
+parOuImpar(2);
