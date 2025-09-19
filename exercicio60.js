6a628689a2130e2186351460fe49c1f9cf6a4c6f
Poliana <polianabadio@gmail.com>
2025-09-18 23:11:47 -0300

    Escreva uma função que receba um array de números e retorne um novo array com todos

---
+++ exercicio60.js
@@ -0,0 +1,9 @@
+/*Escreva uma função que receba um array de números e retorne um novo array com todos 
+os números ao quadrado.*/
+
+function valoresAoQuadrado(numeros){
+    let numerosAoQuadrado = numeros.map(valor=> valor**2);
+    return numerosAoQuadrado;
+}
+
+valoresAoQuadrado([2,6,9,1,3,5])
