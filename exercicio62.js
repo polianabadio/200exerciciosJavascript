2caf119b9a7c9aa9ed71d913ed7c5075df3da27d
Poliana <polianabadio@gmail.com>
2025-09-19 17:04:02 -0300

    Escreva uma função que receba um número e retorne uma string repetida aquele número

---
+++ exercicio62.js
@@ -0,0 +1,10 @@
+/*Escreva uma função que receba um número e retorne uma string repetida aquele número
+ de vezes.*/
+
+ function loop(num){
+    for(let i=0;i<num;i++){
+        console.log(`${num} loop`);
+    }
+ }
+
+ loop(3);
