3b641f02b9aea583de49d6aa66d9a6a98b798916
Poliana <polianabadio@gmail.com>
2025-10-05 10:16:06 -0300

    ajeitando a 65.ts

--- EXERCICIOSTypescript/exercicio65.ts
+++ EXERCICIOSTypescript/exercicio65.ts
@@ -1,11 +1,13 @@
 /*Escreva uma função que aceite um array de números e uma função de callback e retorne 
 a soma de todos os números do array após a aplicação da função de callback.*/
 
-function soma(numeros:number[]){
+function soma(numeros:number[]):number{
     let soma = numeros.reduce((acumulador,valorAtual) => acumulador + valorAtual,0);
     return soma;
 }
 
-function somaArray(numeros: number[], callback: (nums: number[]) => number): number {
+function somaArray(numeros:number[],callback: (nums: number[]) => number): number{
     return callback(numeros);
-  }
+}
+
+somaArray([2,5,10,14,3,1],soma);
