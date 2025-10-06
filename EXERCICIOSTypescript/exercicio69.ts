6f64eed1f285b38f0c238a5e4badb4f77efe48bd
Poliana Ferreira Abadio <polianabadio@gmail.com>
2025-10-06 11:45:31 -0300

    ex 69

--- EXERCICIOSTypescript/exercicio69.ts
+++ EXERCICIOSTypescript/exercicio69.ts
@@ -1,1 +1,13 @@
 //Escreva uma função que aceite um número e uma função de callback, e execute a função de callback o número de vezes especificado.
+
+function impressao(num:number):void{
+    console.log( `--${num}--`);
+}
+
+function loop(num:number,callback:(num:number)=>void):void{
+    for(let i:number=0;i<num;i++){
+        callback(num);
+    }
+}
+
+console.log(loop(2,impressao));
---
+++ EXERCICIOSTypescript/exercicio70.ts
@@ -0,0 +1,3 @@
+/*Escreva uma função de alta ordem que aceite um array de funções de callback e um valor, e retorne um array de resultados de aplicar cada função de 
+callback ao valor.*/
+
