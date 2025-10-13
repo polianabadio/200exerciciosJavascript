d6cc136e21b44ef556d271187901799c1dafcf15
Poliana <polianabadio@gmail.com>
2025-10-13 15:35:06 -0300

    72 e 73

--- EXERCICIOSTypescript/exercicio72.ts
+++ EXERCICIOSTypescript/exercicio72.ts
@@ -2,3 +2,25 @@
 valor inicial, e retorne o resultado final após a aplicação de todas as funções de 
 callback no valor inicial, o número de vezes especificado.*/
 
+const estudo: ((nivel:number) => number) [] = [
+    (nivel) => nivel * 1.1,
+    (nivel) => nivel + 5,
+    (nivel) => nivel * 1.2,
+];
+
+function simularEstudo(vezes: number, callbacks: ((n: number) => number)[], 
+valorInicial: number) {
+    let nivel = valorInicial;
+
+    for (let i = 0; i < vezes; i++) {
+        for (let callback of callbacks) {
+          nivel = callback(nivel);
+        }
+      }
+    
+      return nivel;
+}
+
+const resultado = simularEstudo(3, estudo, 10);
+
+console.log(`Nível final de conhecimento após 3 ciclos: ${resultado.toFixed(2)}`);
--- EXERCICIOSTypescript/exercicio73.ts
+++ EXERCICIOSTypescript/exercicio73.ts
@@ -1,3 +1,23 @@
 /*Escreva uma função de alta ordem que aceite um número, um array de funções de 
 callback e um valor inicial, e retorne o resultado final após a aplicação de todas 
 as funções de callback no valor inicial, a cada "n" milissegundos.*/
+
+const tabuada:((num:number) => number)[] = [
+    (num) => num*0,
+    (num) => num*1,
+    (num) => num*2,
+    (num) => num*3,
+    (num) => num*4,
+    (num) => num*5,
+    (num) => num*6,
+    (num) => num*7,
+    (num) => num*8,
+    (num) => num*9,
+    (num) => num*10,
+]
+
+function executarTabuada(num:number,segundos:number,callbacks:(num:number[])=>number){
+    setTimeout(() => {
+        
+      }, segundos*1000);
+}
